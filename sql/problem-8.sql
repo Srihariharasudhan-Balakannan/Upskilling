5ad6428ad4a93f2e4a4974976e4e55d01d14fad7
Srihariharasudhan Balakannan <139725379+Srihariharasudhan-Balakannan@users.noreply.github.com>
2025-03-11 03:17:12 +0530

    Update problem-8.sql

--- sql/problem-8.sql
+++ sql/problem-8.sql
@@ -1,25 +1,27 @@
--- Problem 8  
--- Find Customers with at Least 3 Orders in the Last 6 Months  
+-- Problem 8
+-- Find customers who placed at least 3 orders in the last 6 months.
 
--- Create orders table  
-CREATE TABLE orders (  
-order_id INT AUTO_INCREMENT PRIMARY KEY,  
-customer_id INT,  
-order_date DATE,  
-amount INT  
-);  
+-- Create orders table
+CREATE TABLE orders (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  amount INT
+);
 
--- Insert data in orders  
-INSERT INTO orders (customer_id, order_date, amount) VALUES  
-(101, '2024-01-10', 1000),  
-(102, '2024-02-15', 2000),  
-(101, '2024-03-20', 1500),  
-(103, '2024-04-05', 2500),  
-(101, '2024-05-08', 3000);  
+-- Insert data in orders
+INSERT INTO orders (customer_id, order_date, amount) VALUES
+(101, '2024-09-15', 1000),
+(102, '2024-10-12', 2000),
+(101, '2024-11-20', 1500),
+(103, '2024-12-05', 2500),
+(101, '2025-01-08', 3000),
+(102, '2025-02-18', 3500),
+(101, '2025-03-02', 4000);
 
--- Expected output  
--- +-------------+-------------+  
--- | customer_id | order_count |  
--- +-------------+-------------+  
--- | 101         |      3      |  
--- +-------------+-------------+  
+-- Expected output
+-- +-------------+--------------+
+-- | customer_id | total_orders |
+-- +-------------+--------------+
+-- | 101         | 4            |
+-- +-------------+--------------+
